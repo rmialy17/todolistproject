053dbecc46c723a1ac07be6c170b3f7e6a008f3a
miandri <mialy.razaf@gmail.com>
2022-04-13 17:23:49 +0200

    Fix some bugs in SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,35 +2,35 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
      */
-    public function loginAction(Request $request)
+    public function loginAction (AuthenticationUtils $util): Response
     {
-        $authenticationUtils = $this->get('security.authentication_utils');
 
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig', [
+            "lastUserName" => $util->getLastUsername(),
+            "error" => $util->getLastAuthenticationError(),   
+        ]);
 
-        return $this->render('security/login.html.twig', array(
-            'last_username' => $lastUsername,
-            'error'         => $error,
-        ));
+        $this->addFlash('success', "Connexion réussie.");
+        return $this->redirectToRoute('homepage');
     }
 
-    /**
-     * @Route("/login_check", name="login_check")
-     */
-    public function loginCheck()
-    {
-        // This code is never executed.
-    }
+    // /**
+    //  * @Route("/login_check", name="login_check")
+    //  */
+    // public function loginCheck()
+    // {
+    //     // This code is never executed.
+    // }
 
     /**
      * @Route("/logout", name="logout")
